9370a6fea45d3e8d9c71def5f011965d4015c54a
Walt Mayfield <waltmayf@amazon.com>
2025-08-25 10:30:03 -0500

    Reveted changes to package.json

--- next.config.js
+++ next.config.js
@@ -1,67 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Temporarily disable type checking during build to speed up builds
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  
-  // Disable ESLint during builds (run separately)
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  
-  // Enable SWC minification for faster builds
-  swcMinify: true,
-  
-  // Optimize webpack configuration
-  webpack: (config, { dev, isServer }) => {
-    // Reduce memory usage during builds
-    if (!dev && !isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        cacheGroups: {
-          default: false,
-          vendors: false,
-          // Vendor chunk for large dependencies
-          vendor: {
-            name: 'vendor',
-            chunks: 'all',
-            test: /node_modules/,
-            priority: 20
-          },
-          // AWS SDK chunk (large dependency)
-          aws: {
-            name: 'aws',
-            chunks: 'all',
-            test: /node_modules\/@aws-sdk/,
-            priority: 30
-          },
-          // LangChain chunk (large dependency)
-          langchain: {
-            name: 'langchain',
-            chunks: 'all',
-            test: /node_modules\/@langchain/,
-            priority: 30
-          },
-          // MUI chunk (large dependency)
-          mui: {
-            name: 'mui',
-            chunks: 'all',
-            test: /node_modules\/@mui/,
-            priority: 30
-          }
-        }
-      };
-    }
-    
-    return config;
-  },
-  
-  // Experimental features for better performance
-  experimental: {
-    // Enable webpack build worker for faster builds
-    webpackBuildWorker: true,
-  }
+  /* config options here */
 };
 
-module.exports = nextConfig;
+module.exports = nextConfig; 
